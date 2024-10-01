5291a23b74639763e29bed9195dcfdb418175c8c
mnv121 <valenciamaynardn@gmail.com>
2024-10-01 14:56:55 +0800

    incentive fix not displaying in history

--- src/org/rmj/fund/manager/base/Incentive.java
+++ src/org/rmj/fund/manager/base/Incentive.java
@@ -3164,9 +3164,9 @@ public class Incentive {
 //            System.out.println("department  = " + p_oApp.getDepartment());
             if ((AUDITOR + "»" + COLLECTION + "»" + FINANCE + "»" + MIS).contains(p_oApp.getDepartment())) {
                 if (p_oApp.getDepartment().equals(AUDITOR)) {
-                    lsCondition = lsCondition + " AND a.cApprovd2 = '0'";
+                    lsCondition = lsCondition + " AND a.cApprovd2 IN ('0','1')";
                 } else if (p_oApp.getDepartment().equals(COLLECTION)) {
-                    lsCondition = lsCondition + " AND a.cApprovd1 = '0'";
+                    lsCondition = lsCondition + " AND a.cApprovd1 IN ('0','1')";
                 }
             } else {
                 lsCondition = lsCondition + " AND LEFT(a.sTransNox,4) = " + SQLUtil.toSQL(p_oApp.getBranchCode());
